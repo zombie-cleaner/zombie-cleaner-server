b8cc71ad5c4e6ca35d8a2f054feeee2028fdf630
smit dhimar <smitdhimar61@gmail.com>
2025-12-27 03:23:56 -0500

    feat: add initial dependency and structure from start.spring.io

---
+++ src/main/java/com/zombie_cleaner/zombie_cleaner_server/ZombieCleanerServerApplication.java
@@ -0,0 +1,13 @@
+package com.zombie_cleaner.zombie_cleaner_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZombieCleanerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZombieCleanerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zombie_cleaner/zombie_cleaner_server/ZombieCleanerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zombie_cleaner.zombie_cleaner_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZombieCleanerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
