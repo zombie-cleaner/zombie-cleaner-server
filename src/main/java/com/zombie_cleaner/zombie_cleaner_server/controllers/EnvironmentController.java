35d970dee844025ec315a22bf84184cdb439ba87
smit dhimar <smitdhimar61@gmail.com>
2026-03-11 23:53:31 -0400

    feat: capsulate response in the response entity

--- src/main/java/com/zombie_cleaner/zombie_cleaner_server/controllers/EnvironmentController.java
+++ src/main/java/com/zombie_cleaner/zombie_cleaner_server/controllers/EnvironmentController.java
@@ -1,8 +1,11 @@
 package com.zombie_cleaner.zombie_cleaner_server.controllers;
 
+import com.zombie_cleaner.zombie_cleaner_server.dtos.ApiResponse;
 import com.zombie_cleaner.zombie_cleaner_server.entities.Environment;
 import com.zombie_cleaner.zombie_cleaner_server.services.EnvironmentService;
+import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +17,15 @@ public class EnvironmentController {
     private EnvironmentService environmentService;
 
     @GetMapping("/api/environment/{id}")
-    public Environment getEnvironment(@PathVariable String id) {
-        return environmentService.getEnvironmentById(id);
+    public ResponseEntity<@NonNull ApiResponse<Environment>> getEnvironmentById(@PathVariable String id) {
+        Environment environment = environmentService.getEnvironmentById(id);
+        if (environment != null) {
+            ApiResponse<Environment> response = ApiResponse.success(environment, "Environment retrieved successfully");
+            return ResponseEntity.ok(response);
+        } else {
+            ApiResponse<Environment> response = ApiResponse.failure("Environment not found");
+            return ResponseEntity.status(404).body(response);
+        }
     }
+
 }
