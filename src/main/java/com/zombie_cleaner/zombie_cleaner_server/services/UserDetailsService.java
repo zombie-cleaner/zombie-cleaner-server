232c8a156e4003ad028a55729456ed9026a5b466
smit dhimar <smitdhimar61@gmail.com>
2026-05-25 12:43:59 -0400

    refactor: remove unnecessory import

--- src/main/java/com/zombie_cleaner/zombie_cleaner_server/services/UserDetailsService.java
+++ src/main/java/com/zombie_cleaner/zombie_cleaner_server/services/UserDetailsService.java
@@ -1,6 +1,6 @@
 package com.zombie_cleaner.zombie_cleaner_server.services;
 
-import com.zombie_cleaner.zombie_cleaner_server.entities.User;import org.springframework.security.core.userdetails.UserDetails;
+import com.zombie_cleaner.zombie_cleaner_server.entities.User;
 
 public interface UserDetailsService {
     public User loadUserByEmail(String email);
